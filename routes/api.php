b5c6ae08065134353547c8e54eed86078c1afaad
pamallika <sasha.fedorenko4736@gmail.com>
2023-04-02 01:56:30 +0300

    add controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AccountController;
+use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\LeadController;
+use App\Http\Controllers\Api\PipelineController;
+use App\Http\Controllers\Api\StageController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +24,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'accounts' => AccountController::class,
+]);
+Route::apiResources([
+    'users' => UserController::class,
+]);
+Route::apiResources([
+    'companies' => CompanyController::class,
+]);
+Route::apiResources([
+    'contacts' => ContactController::class,
+]);
+Route::apiResources([
+    'leads' => LeadController::class,
+]);
+Route::apiResources([
+    'pipelines' => PipelineController::class,
+]);
+Route::apiResources([
+    'stages' => StageController::class,
+]);
+
